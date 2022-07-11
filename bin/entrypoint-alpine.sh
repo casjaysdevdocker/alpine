bb8524c9e934dfe4141bc43df7aa62133910659d
Jason <git-admin@casjaysdev.com>
2022-07-11 17:26:12 -0400

    🗃️  modified: bin/entrypoint-alpine.sh 🗃️

--- bin/entrypoint-alpine.sh
+++ bin/entrypoint-alpine.sh
@@ -1,47 +1,111 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version       : 202202021753-git
-# @Author        : Jason Hempstead
-# @Contact       : jason@casjaysdev.com
-# @License       : WTFPL
-# @ReadME        : entrypoint.sh --help
-# @Copyright     : Copyright: (c) 2022 Jason Hempstead, Casjays Developments
-# @Created       : Wednesday, Feb 02, 2022 17:53 EST
-# @File          : entrypoint.sh
-# @Description   :
-# @TODO          :
-# @Other         :
-# @Resource      :
+##@Version           :  202207111721-git
+# @Author            :  Jason Hempstead
+# @Contact           :  jason@casjaysdev.com
+# @License           :  LICENSE.md
+# @ReadME            :  entrypoint-alpine.sh --help
+# @Copyright         :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @Created           :  Monday, Jul 11, 2022 17:21 EDT
+# @File              :  entrypoint-alpine.sh
+# @Description       :  
+# @TODO              :  
+# @Other             :  
+# @Resource          :  
+# @sudo/root         :  no
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPNAME="$(basename "$0")"
-VERSION="202202021753-git"
-USER="${SUDO_USER:-${USER}}"
-HOME="${USER_HOME:-${HOME}}"
+APPNAME="$(basename "$0" 2>/dev/null)"
+VERSION="202207111721-git"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
 if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
-trap 'exitCode=${exitCode:-$?};[ -n "$ENTRYPOINT_SH_TEMP_FILE" ] && [ -f "$ENTRYPOINT_SH_TEMP_FILE" ] && rm -Rf "$ENTRYPOINT_SH_TEMP_FILE" &>/dev/null' EXIT
-__exec_bash() { [ -n "$1" ] && exec /bin/bash -l -c "${@:-bash}" || exec /bin/bash -l; }
+trap 'exit 0' SIGINT SIGTERM
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-HOSTNAME="$(hostname -f)"
-export TZ="${TZ:-America/New_York}"
-export HOSTNAME="${HOSTNAME:-casjaysdev-alpine}"
+# Set functions
+__exec_bash() {
+  local cmd="${*:-/bin/bash -l}"
+  exec $cmd || return 10
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__find() { ls -A "$*" 2>/dev/null; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Define default variables
+TZ="${TZ:-America/New_York}"
+HOSTNAME="${HOSTNAME:-casjaysdev-bin}"
+BIN_DIR="${BIN_DIR:-/usr/local/bin}"
+DATA_DIR="${DATA_DIR:-$(__find /data/ 2>/dev/null | grep '^' || false)}"
+CONFIG_DIR="${CONFIG_DIR:-$(__find /config/ 2>/dev/null | grep '^' || false)}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional variables
 
-[ -n "${TZ}" ] && echo "${TZ}" >/etc/timezone
-[ -n "${HOSTNAME}" ] && echo "${HOSTNAME}" >/etc/hostname
-[ -n "${HOSTNAME}" ] && echo "127.0.0.1 $HOSTNAME localhost" >/etc/hosts
-[ -f "/usr/share/zoneinfo/${TZ}" ] && ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Export variables
+export TZ HOSTNAME
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set timezone
+[[ -n "${TZ}" ]] && echo "${TZ}" >/etc/timezone
+[[ -f "/usr/share/zoneinfo/${TZ}" ]] && ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set hostname
+if [[ -n "${HOSTNAME}" ]]; then
+  echo "${HOSTNAME}" >/etc/hostname
+  echo "127.0.0.1 ${HOSTNAME} localhost ${HOSTNAME}.local" >/etc/hosts
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Delete any gitkeep files
+[[ -n "${CONFIG_DIR}" ]] && { [[ -d "${CONFIG_DIR}" ]] && rm -Rf "${CONFIG_DIR}/.gitkeep" || mkdir -p "/config/"; }
+[[ -n "${DATA_DIR}" ]] && { [[ -d "${DATA_DIR}" ]] && rm -Rf "${DATA_DIR}/.gitkeep" || mkdir -p "/data/"; }
+[[ -n "${BIN_DIR}" ]] && { [[ -d "${BIN_DIR}" ]] && rm -Rf "${BIN_DIR}/.gitkeep" || mkdir -p "/bin/"; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy config files to /etc
+if [[ -n "${CONFIG_DIR}" ]]; then
+  for config in ${CONFIG_DIR}; do
+    if [[ -d "/config/$config" ]]; then
+      [[ -d "/etc/$config" ]] || mkdir -p "/etc/$config"
+      cp -Rf "/config/$config/." "/etc/$config/"
+    elif [[ -f "/config/$config" ]]; then
+      cp -Rf "/config/$config" "/etc/$config"
+    fi
+  done
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional commands
 
-case $1 in
-healthcheck)
-  echo 'OK'
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+case "$1" in
+--help) # Help message
+  echo 'Docker container for '$APPNAME''
+  echo "Usage: $APPNAME [healthcheck, bash, command]"
+  echo "Failed command will have exit code 10"
+  echo
+  exitCode=$?
   ;;
-bash | shell | sh)
+
+healthcheck) # Docker healthcheck
+  echo "$(uname -s) $(uname -m) is running"
+  echo _other_commands here
+  exitCode=$?
+  ;;
+
+*/bin/sh | */bin/bash | bash | shell | sh) # Launch shell
   shift 1
-  __exec_bash "$@"
+  echo "running command: $* in bash"
+  __exec_bash "${@:-/bin/bash}"
+  exitCode=$?
   ;;
-*)
-  __exec_bash "$@"
+
+*) # Execute primary command
+  echo "running command: $*"
+  __exec_bash "${@:-/bin/bash}"
+  exitCode=$?
   ;;
 esac
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end of entrypoint
+exit ${exitCode:-$?}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
